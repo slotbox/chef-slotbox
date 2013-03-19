aa240a0de6e029cfb006cd4e005da3a5c4686990
Thomas Buckley-Houston <tom@tombh.co.uk>
2013-03-19 10:49:09 +0000

    use line break in sudoers

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ chef_version="10.20"
 
 function add_rukosan_user {
 	sudo useradd -d /home/rukosan -m rukosan -s /bin/bash -c Rukosan &&
-	sudo su - -c "echo \"rukosan ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers"
+	sudo su - -c "echo \"\nrukosan ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers"
 }
 
 if [ "$DRONEIO" != "true" ]; then
