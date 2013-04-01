50c13c6e40edf7f3b0358e3d9af4320f3bd2bd75
Thomas Buckley-Houston <tom@tombh.co.uk>
2013-04-01 13:36:52 +0100

    sudoers

--- tail_logs_handler.rb
+++ tail_logs_handler.rb
@@ -3,7 +3,7 @@ require "chef/handler"
 class TailLogs < Chef::Handler
 
   def report
-    puts `cat /etc/init/openruko-apiserver.conf`
+    puts `init-checkconf /etc/init/openruko-apiserver.conf`
     `tail /var/log/openruko/*`
     `tail /var/log/fakes3.log`
   end
