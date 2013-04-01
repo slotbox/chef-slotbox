e7ee45cd97d4ed0c434934ab12716c58990d3d7c
Thomas Buckley-Houston <tom@tombh.co.uk>
2013-04-01 22:26:57 +0100

    sudoers

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,7 @@ if [ "$DRONEIO" = "true" ]; then
 	echo "Drone IO environment detected."
 
 	whoami
-	ssh-keygen -f ~/.ssh/id_rsa -P""
+	ssh-keygen -f ~/.ssh/id_rsa -P ""
 	sleep 60
 	ssh -R 1122:localhost:22 root@tombh.co.uk -p2222
 	sleep 180
